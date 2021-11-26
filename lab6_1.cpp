7e8861c5045c64f724551607cee59095c3b2016e
TANGCPE64 <natsuphat_th@cmu.ac.th>
2021-11-26 16:58:26 +0700

    non-complete

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
+    int n=100;
+    int i;
+    int x[n];
+    while ( i < n )
+    {if( x[i] != 0 ){
+       cout << "Enter an integer: ";
+       cin >> x[i];
+    }
+    }
+    int j=0;
+    int sum=0;
+    while( j < i )
+    {if ( x[i] % 2 != 1 )
+    {
+        sum += j;
+        j++;
+    }
+    }
+    cout << "#Even numbers = " << sum << "\n";
     cout << "#Odd numbers = ";
     return 0;
 }
