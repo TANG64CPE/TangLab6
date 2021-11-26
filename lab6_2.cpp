15793030052f8b78f1fc153ab455d58bd6044f94
TANGCPE64 <natsuphat_th@cmu.ac.th>
2021-11-26 17:41:09 +0700

    non-complete

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+double deg2rad (double deg){
+    return deg * M_PT / 180.0;
+}
+ 
+double findXComponent(double l1,double l2,double a1,double a2){
+    return 0.0;
+}
 
+double findYComponent(double l1,double l2,double a1,double a2){
+    return 0.0;
+}
+
+double pythagoras(double xcomp,double ycomp){
+    return 0.0;
+}
+
+double atan2 (double x,double y){
+    return atan2(x,y)*180.0 / M_PI;
+}
+
+double rad2deg(double atan2,double ycomp,double xcomp){
+    return 0.0;
+}
+
+double showResult(double result_vec_length,double result_vec_direction)
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
